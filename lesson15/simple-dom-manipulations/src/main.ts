1502f9e3a17ffed0a9835cf8c71d2dd91ab50b96
Talgat <talgat.s@protonmail.com>
2023-08-05 10:08:38 +0600

    Add lesson16

--- lesson15/simple-dom-manipulations/src/main.ts
+++ lesson15/simple-dom-manipulations/src/main.ts
@@ -3,66 +3,28 @@ import "./style.css";
 const ctrl = new AbortController();
 const signal = ctrl.signal;
 
-window.addEventListener(
-  "load",
-  () => {
-    // const btn = document.getElementById("click-me") as HTMLButtonElement;
-    // const btnRemove = document.getElementById(
-    //   "remove-event-listener",
-    // ) as HTMLButtonElement;
-    //
-    // function greetings(event: MouseEvent) {
-    //   console.log("target", event.target);
-    //   console.log("currentTarget", event.currentTarget);
-    //
-    //   const str = "Hey Yo!";
-    //   alert(str);
-    // }
-    //
-    // function removeEventListener() {
-    //   ctrl.abort();
-    // }
-    //
-    // if (btn) {
-    //   btn.addEventListener("click", greetings, { signal });
-    // }
-    //
-    // if (btnRemove) {
-    //   btnRemove.addEventListener("click", removeEventListener);
-    // }
+window.addEventListener("load", () => {
+  const btn = document.getElementById("btn");
 
-    const container = document.getElementById("container");
-    const child = document.getElementById("child");
+  function alertOne() {
+    alert("1");
+  }
 
-    if (container) {
-      container.addEventListener(
-        "click",
-        (event: MouseEvent) => {
-          console.log("parent target", event.target);
-          console.log("parent currentTarget", event.currentTarget);
-        },
-        { signal },
-      );
-    }
+  if (btn) {
+    btn.addEventListener("click", alertOne, { signal });
+    btn.addEventListener("click", () => alert("3"), { signal });
+    btn.addEventListener("click", () => alert("4"));
 
-    if (child) {
-      child.addEventListener(
-        "click",
-        (event: MouseEvent) => {
-          console.log("child target", event.target);
-          console.log("child currentTarget", event.currentTarget);
-        },
-        { signal },
-      );
-    }
-  },
-  true,
-);
+    ctrl.abort();
+
+    btn.onclick = () => alert(2);
+  }
+});
 
 window.addEventListener(
   "unload",
   () => {
-    ctrl.abort();
+    // ctrl.abort();
   },
   true,
 );
