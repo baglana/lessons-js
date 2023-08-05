b021f55e00c3cc758ae310ba577d70afbea0b413
Talgat <talgat.s@protonmail.com>
2023-08-05 11:20:10 +0600

    Update lesson16

--- lesson16/dom-handlers-sample/src/main.ts
+++ lesson16/dom-handlers-sample/src/main.ts
@@ -1,24 +1,29 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+window.addEventListener("load", loadHandler, { once: true });
+window.addEventListener("unload", unloadHandler, { once: true });
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+function loadHandler() {
+  const form = document.getElementById("login-form");
+
+  form?.addEventListener("submit", (event: SubmitEvent) => {
+    event.preventDefault();
+    const { currentTarget } = event;
+    if (currentTarget instanceof HTMLFormElement) {
+      console.log("currentTarget.elements", currentTarget.elements);
+
+      const formData = new FormData(currentTarget);
+
+      console.log(formData.get("user-email"));
+
+      // const emailEl = currentTarget.elements.namedItem("user-email");
+      // if (emailEl instanceof HTMLInputElement) {
+      //   console.log("emailEl?.value", emailEl?.value);
+      // }
+    }
+  });
+}
+
+function unloadHandler() {
+  return null;
+}
