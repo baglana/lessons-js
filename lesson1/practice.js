061da2c68f21a674563726d8ef0c5b18b72b23fa
Talgat <talgat.s@protonmail.com>
2023-08-24 20:10:50 +0600

    update lesson1

---
+++ lesson1/practice.js
@@ -0,0 +1,69 @@
+// // mutable variables
+// let myName;
+// let myAge;
+//
+// myName = "I am Batman";
+//
+// // // immutable variables
+// // const age;
+// //
+// // // initialize variables
+// const myCat = "Garfield";
+//
+// myCat = "Puss in Boots"; // Error
+
+// let num = 2e3;
+
+// let a = 5;
+// let b = a;
+// b = 10;
+//
+// console.log(a === b); // true
+//
+// b = 10;
+//
+// console.log(a === b); // false
+//
+// [5][5][10][][][][]
+//  |     |
+//  a     b
+
+// let wizard1 = {
+//   name: "Harry Potter",
+// };
+//
+// let wizard2 = {
+//   name: "Ron Weasley",
+// };
+//
+// console.log(wizard1 === wizard2);
+//
+// [{...}][{...}][][][][][]
+// 		|     |
+//  	wiz1   wiz2
+
+// let wizard1 = {
+//   name: "Harry Potter",
+// };
+//
+// let wizard2 = wizard1;
+//
+// wizard2.name = "Ron Weasley";
+//
+// console.log(wizard1 === wizard2);
+// console.log(wizard1);
+// console.log(wizard2);
+
+// [{name: "Ron Weasley"}][][][][][][]
+//    |  \
+// wiz1   wiz2
+//
+// let a = 3;
+// a = 5;
+//
+// console.log(a);
+
+//
+// console.log(1 === "1");
+
+console.log("Hello!");
