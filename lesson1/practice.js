663afcbde5be7dc98a4d811a1c671c3c9785e48f
baglana <102351104+baglana@users.noreply.github.com>
2023-08-28 17:18:27 +0600

    Finish lesson1/ homework

---
+++ lesson1/practice.js
@@ -0,0 +1,28 @@
+const prompt = require('prompt-sync')();
+
+const operand1 = +prompt("Please type first number ");
+console.log(`First number ${operand1}!`);
+
+const operator = prompt("Please type operator ");
+console.log(`Operator ${operator}!`);
+
+const operand2 = +prompt("Please type second number ");
+console.log(`Second number ${operand1}!`);
+
+console.log(`Result is ${compute(operator, operand1, operand2)}`);
+
+function compute(operator, operand1, operand2) {
+  switch(operator) {
+    case '+':
+      return operand1 + operand2;
+    case '-':
+      return operand1 - operand2;
+    case '/':
+      return operand1 / operand2;
+    case '*':
+      return operand1 * operand2;
+    default:
+      console.log("Operator isn't in {+, -, / , *}")
+      break;
+  }
+}
