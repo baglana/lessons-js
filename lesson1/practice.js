887c285b6253e76a11c983ce29a3d152896f378c
baglana <102351104+baglana@users.noreply.github.com>
2023-09-16 13:50:58 +0600

    Rename variables and add space after switch

--- lesson1/practice.js
+++ lesson1/practice.js
@@ -1,31 +1,31 @@
 const prompt = require('prompt-sync')();
 
-const operand1 = +prompt("Please type first number ");
-console.log(`First number ${operand1}!`);
+const num1 = +prompt("Please type first number ");
+console.log(`First number ${num1}!`);
 
 const operator = prompt("Please type operator ");
 console.log(`Operator ${operator}!`);
 
-const operand2 = +prompt("Please type second number ");
-console.log(`Second number ${operand1}!`);
+const num2 = +prompt("Please type second number ");
+console.log(`Second number ${num1}!`);
 
-console.log(`Result is ${compute(operator, operand1, operand2)}`);
+console.log(`Result is ${compute(operator, num1, num2)}`);
 
-function compute(operator, operand1, operand2) {
-  switch(operator) {
+function compute(operator, num1, num2) {
+  switch (operator) {
     case '+':
-      return operand1 + operand2;
+      return num1 + num2;
     case '-':
-      return operand1 - operand2;
+      return num1 - num2;
     case '/':
-      if (operand2 === 0) {
+      if (num2 === 0) {
         console.log("You can't divide by zero");
         return;
       } else {
-        return operand1 / operand2;
+        return num1 / num2;
       }
     case '*':
-      return operand1 * operand2;
+      return num1 * num2;
     default:
       console.log("Operator isn't in {+, -, / , *}")
       break;
